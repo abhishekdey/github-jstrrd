fc715b933de2d6a8af00c5f86ac375d12223044e
abhishekdey <abhishekdey.sdm@gmail.com>
2022-04-01 12:10:16 +0530

    3 files modified, 2 added

--- app1/src/app.module.ts
+++ app1/src/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './App.component';
 
 @NgModule({
   declarations: [AppComponent],
--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -1,42 +1,43 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {ModuleFederationPlugin} = require("webpack").container;
-const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index",
-  mode: "development",
+  entry: './src/index',
+  mode: 'development',
   devServer: {
-    static: path.join(__dirname, "dist"),
+    static: path.join(__dirname, 'dist'),
     port: 3001,
   },
   output: {
-    publicPath: "auto",
+    publicPath: 'auto',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-react"],
+          presets: ['@babel/preset-react'],
         },
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "app1",
+      name: 'app1',
       remotes: {
-        app2: "app2@[app2Url]/remoteEntry.js",
+        app2: 'app2@[app2Url]/remoteEntry.js',
       },
-      shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+      shared: {
+        angular: { singleton: true },
+      },
     }),
     new ExternalTemplateRemotesPlugin(),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
-
