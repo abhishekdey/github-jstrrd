256c240685b46ef4ece0eddcb1a865ecee4600e4
abhishekdey <abhishekdey.sdm@gmail.com>
2022-04-01 11:58:49 +0530

    Initial commit

---
+++ app1/src/App.component.js
@@ -0,0 +1,28 @@
+// import React, {Suspense} from "react";
+// const RemoteApp = React.lazy(() => import("app2/App"));
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div style={{
+//         margin:"10px",
+//         padding:"10px",
+//         textAlign:"center",
+//         backgroundColor:"greenyellow"
+//       }}>
+//         <h1>App1</h1>
+//       </div>
+//       <Suspense fallback={"loading..."}>
+//         <RemoteApp/>
+//       </Suspense>
+//     </div>)
+// }
+
+// export default App;
+
+import { Component } from '@angular/common';
+@Component({
+  template: '<div> App Component</div>',
+  selector: 'app-component',
+})
+export class AppComponent {}
