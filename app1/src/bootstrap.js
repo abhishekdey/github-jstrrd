c49e439732f73715d6f0e3c12528d506b68a7f76
abhishekdey <abhishekdey.sdm@gmail.com>
2022-04-01 12:38:50 +0530

    1 file modified

--- app1/src/bootstrap.js
+++ app1/src/bootstrap.js
@@ -1,5 +1,18 @@
-import App from './App.component';
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import App from './App.component';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+// import { environment } from './environments/environment';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
